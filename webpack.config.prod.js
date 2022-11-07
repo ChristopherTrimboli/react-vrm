5f1964ae843a0bedbbc79419fe22202a68452d7b
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-06 17:33:38 -0700

    add dev / prod webpack configs

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './src/index.tsx',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.join(__dirname, "index.html"),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "vrms/", to: "vrms/" },
+            ],
+        }),
+    ],
+};
