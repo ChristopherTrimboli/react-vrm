67566d9ec1d25204f26b9173f41f19090bbad3e5
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-06 17:06:44 -0700

    add Floor

---
+++ src/Floor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { DoubleSide } from "three";
+
+const Floor = () => {
+    return <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[20, 20, 50, 50]} />
+        <meshBasicMaterial color="hotpink" side={DoubleSide} wireframe />
+    </mesh>
+}
+
+export default Floor;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import VRM from './VRM';
 import OrbitCamera from './OrbitCamera';
+import Floor from './Floor';
 
 const root = document.getElementById('root');
 if (root) (
   createRoot(root).render(
     <Canvas>
+      <Floor />
       <OrbitCamera />
       <VRM url="../vrms/setuna.vrm" />
     </Canvas>,
